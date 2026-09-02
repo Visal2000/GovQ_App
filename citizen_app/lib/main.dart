06457a2262039c06e7f63108ecba169ffcbcccde
Visal Hewage <visalsathsara55@gmail.com>
2026-09-02 11:23:04 +0530

    android app error fixed

--- citizen_app/lib/main.dart
+++ citizen_app/lib/main.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'theme/app_theme.dart';
 import 'screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: "AIzaSyBIaPSEk3nsZRrWo0xjbg3OXlTLuXoqqDk",
-      authDomain: "govq-d6d0c.firebaseapp.com",
-      projectId: "govq-d6d0c",
-      storageBucket: "govq-d6d0c.firebasestorage.app",
-      messagingSenderId: "712217903276",
-      appId: "1:712217903276:web:faf5abcb309f76e181df09",
-      measurementId: "G-81RQ01H4P5",
-    ),
-  );
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyBIaPSEk3nsZRrWo0xjbg3OXlTLuXoqqDk",
+        authDomain: "govq-d6d0c.firebaseapp.com",
+        projectId: "govq-d6d0c",
+        storageBucket: "govq-d6d0c.firebasestorage.app",
+        messagingSenderId: "712217903276",
+        appId: "1:712217903276:web:faf5abcb309f76e181df09",
+        measurementId: "G-81RQ01H4P5",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const GovQApp());
 }
 
